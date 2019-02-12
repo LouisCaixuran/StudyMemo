531ef372190b3fdefe409810b682a39249640a49
Louis <louiscaixuran@163.com>
2019-02-12 20:10:46 +0800

    add Btree.show

--- tree.py
+++ tree.py
@@ -4,9 +4,11 @@ class Node():
         self.lchild=lchild
         self.rchild=rchild
 
+
 class BTree():
     def __init__(self,root=Node()):
         self.root=root
+        self.lists=[]
 
     def add(self,new):
         if self.root.value==None:
@@ -26,11 +28,11 @@ class BTree():
                     stack.append(node.lchild)
                     stack.append(node.rchild)
 
-      def front_digui(self,root):
+    def front_digui(self,root):
         if root==None or root.value==None:
             return
         else:
-            print(root.value)
+            self.lists.append(root.value)
             self.front_digui(root.lchild)
             self.front_digui(root.rchild)
 
@@ -50,3 +52,40 @@ class BTree():
             self.back_digui(root.lchild)
             self.back_digui(root.rchild)   
             print(root.value)
+
+    def show(self):
+        self.front_digui(self.root)
+        i=1
+        j=0
+        k=1
+        l=[]
+        while i<=len(self.lists):
+            l.append(self.lists[i-1])
+            if k ==2**j:
+                j+=1
+                print l
+                l=[]
+                k=0
+            k+=1
+            i+=1
+        print l
+
+
+
+                
+
+
+
+        
+t=BTree()
+t.add(8)
+t.add(9)
+t.add(15)
+t.add(20)
+t.add(2)
+t.add(13)
+t.add(14)
+t.add(25)
+t.show()
+
+
